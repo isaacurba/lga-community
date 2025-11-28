b8ab265fa5b18ddae2b2a6c81edaefc8062a3456
isaacurba <kpomassiurban@gmail.com>
2025-11-28 15:07:16 +0100

    Added isAuthenticated function

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -317,3 +317,11 @@ export const verifyEmail = async (req, res) => {
     return res.json({ success: false, message: error.message });
   }
 };
+
+export const isAuthenticated = async (req, res) => {
+  try {
+    return res.json({ success: true, message: "User Authenticated" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
