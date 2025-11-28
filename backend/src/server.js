7dfc87ba33f1c57a3fbcec30328673d16baa0e44
isaacurba <kpomassiurban@gmail.com>
2025-11-28 14:28:35 +0100

    Updated the routes in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,32 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectedDB from "./config/db.js";
-import authRouter from "../src/routes/authRoutes.js";
+import authRouter from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
+// Fix: Set explicit CORS origin to allow cookies
 app.use(
   cors({
-    // origin: process.env.ALLOWED_ORIGIN,
+    origin: "http://localhost:8080",
     credentials: true,
   })
 );
-// API EndPoints
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("Api Working");
-})
-app.use("/api/v1/auth", authRouter);
+  res.send("API is running...");
+});
+
+app.use("/api/staff/auth", authRouter);
 
 app.listen(PORT, () => {
   connectedDB().then(() => {
