fa55b6877d08fb696e8889742826160310ae1389
isaacurba <kpomassiurban@gmail.com>
2025-12-02 23:59:41 +0100

    updated Auth orutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,20 @@
 import express from "express";
-import { register, login, logOut, sendVerifyOtp, verifyEmail, isAuthenticated, resetPassword, sendResetOtp} from "../controllers/authController.js";
+import {
+  register,
+  login,
+  logOut,
+  sendVerifyOtp,
+  verifyEmail,
+  isAuthenticated,
+  resetPassword,
+  sendResetOtp,
+} from "../controllers/authController.js";
 import userAuth from "../middleware/userAuth.js";
 // Import citizen controllers
-import { citizenLogin, registerCitizen } from "../controllers/citizenController.js";
+import {
+  citizenLogin,
+  registerCitizen,
+} from "../controllers/citizenController.js";
 
 const authRouter = express.Router();
 
@@ -15,9 +27,8 @@ authRouter.post("/is-auth", userAuth, isAuthenticated);
 authRouter.post("/send-reset-otp", sendResetOtp);
 authRouter.post("/reset-password", resetPassword);
 
-
 // Add citizen routes as specified
+authRouter.post("/register-citizen", registerCitizen);
 authRouter.post("/citizen/login", citizenLogin);
-authRouter.post("/staff/register-citizen", registerCitizen);
 
 export default authRouter;
