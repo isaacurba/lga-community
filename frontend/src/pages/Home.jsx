cdd796219a38af2bd5832f5f135c61495a285964
isaacurba <kpomassiurban@gmail.com>
2025-12-09 04:03:35 +0100

    moves home page to root to page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar';
+import Header from "@/components/Header"
+
+const StaffHome = () => {
+  return (
+    <div>
+          <Navbar/>
+          <Header/>
+    </div>
+
+    
+  )
+}
+
+export default StaffHome
