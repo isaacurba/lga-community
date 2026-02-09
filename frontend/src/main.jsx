0c38392328c52749cbd85d94d3aedb56544491f5
isaacurba <kpomassiurban@gmail.com>
2026-02-09 07:02:52 +0100

    refactor(frontend): simplify main entry point structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import { Toaster } from './components/ui/sonner.jsx';
-import {  AppContextProvider } from './context/AppContextProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-     <Toaster richColors position="top-center" />
-    </BrowserRouter>
+    <App />
+    <Toaster richColors position="top-center" />
   </React.StrictMode>,
 );
